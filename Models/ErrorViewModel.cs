0fdbaebfdd9bb56f4bf0263ec21ea2ee84bc54be
Ubuntu <ubuntu@ip-172-31-19-85.ec2.internal>
2025-04-18 17:05:05 +0000

    Initial .NET MVC app with Dockerfile

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnet_devsecops_sample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
